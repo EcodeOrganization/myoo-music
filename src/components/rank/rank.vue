2f4c70a01d9797bb990d74be44bf9ab97ffcaf76
oohlxoogit config --global user.name oohlxoogit config --global user.name <huanglixia2014@163.com>
2018-08-13 17:11:58 +0800

    第一次提交 推荐页面列表不能滚动，原因不明 <huanglixia2014@163.com>

---
+++ src/components/rank/rank.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>rank</div>
+</template>
+
+<script>
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
---
+++ src/components/recommend/test.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="n in number" :key="n" @click="change(n)">{{n}}</li>
+        </ul>
+      
+    </div>
+</template>
+
+<script>
+    export default {
+        data ()  {
+            return {
+              number: [97, 98, 99, 100]
+            }
+        },
+        methods: {
+            change(n) {
+                this.$emit("input", n)
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
+
+
